a041f993018597a51ed9161dfad18b1ae569b1ee
Lars <lars@tftelecke.de>
2022-03-21 11:21:28 +0100

    Add skeleton in main.py

---
+++ src/peartrainer/main.py
@@ -0,0 +1,10 @@
+"""Peartrainer - A musical ear training software written in Python."""
+
+
+def main():
+    """Run controller for Peartrainer and set up software."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
