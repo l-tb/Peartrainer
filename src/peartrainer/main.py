aae09b94bcfbceeced760c7835ea39860696fcd0
Lars <lars@xn--tftelecke-q9a.de>
2022-03-30 11:41:29 +0200

    Add conversion of port choise and open midi port

--- src/peartrainer/main.py
+++ src/peartrainer/main.py
@@ -1,5 +1,6 @@
 """Peartrainer - A musical ear training software written in Python."""
 import rtmidi
+import sys
 
 
 def generate_interval() -> tuple[int]:
@@ -28,6 +29,13 @@ def main() -> None:
     portOptions = portOptions + f"\n({len(availablePorts)}) create Port\n\n> "
     chosenPort = input(portOptions)
 
+    try:
+        port = int(chosenPort)
+    except ValueError:
+        print("Invalid port option.")
+        sys.exit(1)
+    midiout.open_port(port)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
